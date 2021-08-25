084d1566dc624c631250642571d5ef930641b42c
chriskok <pherkok@gmail.com>
2021-08-25 18:38:46 -0500

    Clean up and marker js bug fix

--- webserver.py
+++ webserver.py
@@ -3,12 +3,11 @@ from fastapi.staticfiles import StaticFiles
 
 # Use this to serve a public/index.html
 from starlette.responses import FileResponse 
-from starlette.responses import RedirectResponse  # add this
+from starlette.responses import RedirectResponse
 
 app = FastAPI()
 app.mount("/public", StaticFiles(directory="public"), name="public")
 
 @app.get("/")
 async def read_index():
-    # return FileResponse('public/index.html')  # remove this
-    return RedirectResponse(url="/public/index.html")  # change to this
+    return RedirectResponse(url="/public/index.html")  
