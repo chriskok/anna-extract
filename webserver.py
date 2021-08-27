03e25bd3786e3f6060047e4d33523a25eff194c7
chriskok <pherkok@gmail.com>
2021-08-27 12:47:00 -0500

    Returned data from server to template

--- webserver.py
+++ webserver.py
@@ -1,15 +1,17 @@
 from fastapi import  FastAPI, Form, Request
 from fastapi.staticfiles import StaticFiles
-import json
+from fastapi.templating import Jinja2Templates
 
 # Use this to serve a public/index.html
 from starlette.responses import FileResponse 
 from starlette.responses import RedirectResponse
 
 from processing import *
+import json
 
 app = FastAPI()
 app.mount("/public", StaticFiles(directory="public"), name="public")
+templates = Jinja2Templates(directory="public")
 
 @app.get("/")
 async def read_index():
@@ -26,4 +28,5 @@ def form_post(request: Request, my_hidden_input: str = Form(...)):
         cropped_image = crop_image(parsed_data[label], curr_image_path)
         return_dict[label] = ocr_on_image(cropped_image)
 
-    return return_dict
+    # return return_dict
+    return templates.TemplateResponse('validation.html', context={'request': request, 'result': return_dict})
